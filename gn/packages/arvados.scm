15bebdb05505faddf6669cf1ae46c78e2cda82d8
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2017-06-27 19:43:45 +0300

    Move source definitions into functions
    * Move definitions for possibly common source libraries into
      functions that can be called to build the appropriate input
      definitions.

--- gn/packages/arvados.scm
+++ gn/packages/arvados.scm
@@ -38,6 +38,81 @@
     'setup-go-workspace
     ,(build-make-dirs dirs)))
 
+(define (use-url-fetch name url hash)
+  `(,name
+    ,(origin
+      (method url-fetch)
+      (uri url)
+      (sha256
+       (base32
+	hash)))))
+
+(define (use-git-fetch name uri commit-hash hash)
+  `(,name
+    ,(origin
+      (method git-fetch)
+      (uri
+       (git-reference
+	(url uri)
+	(commit commit-hash)))
+      (sha256
+       (base32
+	hash)))))
+
+(define (build-yaml-src)
+  (use-url-fetch
+   "yaml-src"
+   "https://github.com/ghodss/yaml/archive/v1.0.0.tar.gz"
+   "0i6yjwh3j2184lwwi537q7z666wppf5s1kz1m894d53is5yb8xla"))
+
+(define (build-yaml.v2-src)
+  (use-git-fetch
+   "yaml.v2-src"
+   ;; The url below gets the repo and checks out the
+   ;; correct branch
+   "https://gopkg.in/yaml.v2"
+   "cd8b52f8269e0feb286dfeef29f8fe4d5b397e0b"
+   "1hj2ag9knxflpjibck0n90jrhsrqz7qvad4qnif7jddyapi9bqzl"))
+
+(define (build-goamz-src)
+  (use-git-fetch
+   "goamz-src"
+   "https://github.com/AdRoll/goamz.git"
+   "c5d7d9bd6c743fae44efc6c18450282022445ffc"
+   "0yj1yqnskp1zcmihiq3yxlb9gm3w8ng5radvzjnc60r8vc78ihg2"))
+
+(define (build-go-systemd-src)
+  (use-url-fetch
+   "go-systemd-src"
+   "https://github.com/coreos/go-systemd/archive/v14.tar.gz"
+   "0219hmzbyvrlbarbrs66hpc076hi0hfbk6invvmzrp5x4lda96wx"))
+
+(define (build-azure-sdk-for-go-src)
+  (use-git-fetch
+   "azure-sdk-for-go-src"
+   "https://github.com/curoverse/azure-sdk-for-go.git"
+   "1620af6b32398bfc91827ceae54a8cc1f55df04d"
+   "08fg7wyp1nlz2cfjx8p4phs586fxxabji5r082g5ixxsrq52b600"))
+
+(define (build-mux-src)
+  (use-url-fetch
+   "mux-src"
+   "https://github.com/gorilla/mux/archive/v1.3.0.tar.gz"
+   "0k6kgz9qgl6c1zd271hnzp96yh2p2v0410k4dq0y1j3qg2qfd7m1"))
+
+(define (build-logrus-src)
+  (use-url-fetch
+   "logrus-src"
+   "https://github.com/sirupsen/logrus/archive/v0.11.5.tar.gz"
+   "0skwgvd2d36y9fipjpbkzin9l9pnl2gk1i3xsf27497db5g5mgyj"))
+
+(define (build-check.v1-src)
+  (use-git-fetch
+   "check.v1-src"
+   "https://gopkg.in/check.v1"
+   "20d25e2804050c1cd24a7eea1e7a6447dd0e74ec"
+   "0k1m83ji9l1a7ng8a7v40psbymxasmssbrrhpdv2wl4rhs0nc3np"))
+
 (define arvados-minimal
   (package
    (name "arvados-keepstore")
@@ -117,9 +192,9 @@ supports versioning, reproducibilty, and provenance.
 		   (cp-src "check.v1-src"
 			   (string-append src_dir "/gopkg.in/check.v1"))
 		   (cp-src "azure-sdk-for-go-src"
-			   (string-append
-			    src_dir
-			    "/github.com/curoverse/azure-sdk-for-go"))))))
+		   	   (string-append
+		   	    src_dir
+		   	    "/github.com/curoverse/azure-sdk-for-go"))))))
 	(delete 'configure)
 	(add-before 'build 'copy-source
 	  (lambda* _
@@ -162,76 +237,14 @@ supports versioning, reproducibilty, and provenance.
    (native-inputs
     `(("python-wrapper" ,python-wrapper)))
    (inputs
-    `(("goamz-src"
-       ,(origin
-	  (method git-fetch)
-	  (uri
-	   (git-reference
-	    (url "https://github.com/AdRoll/goamz.git")
-	    (commit "c5d7d9bd6c743fae44efc6c18450282022445ffc")))
-	  (sha256
-	   (base32
-	    "0yj1yqnskp1zcmihiq3yxlb9gm3w8ng5radvzjnc60r8vc78ihg2"))))
-      ("go-systemd-src"
-       ,(origin
-	  (method url-fetch)
-	  (uri "https://github.com/coreos/go-systemd/archive/v14.tar.gz")
-	  (sha256
-	   (base32
-	    "0219hmzbyvrlbarbrs66hpc076hi0hfbk6invvmzrp5x4lda96wx"))))
-      ("azure-sdk-for-go-src"
-       ,(origin
-	  (method git-fetch)
-	  (uri
-	   (git-reference
-	    (url "https://github.com/curoverse/azure-sdk-for-go.git")
-	    (commit "1620af6b32398bfc91827ceae54a8cc1f55df04d")))
-	  (sha256
-	   (base32
-	    "08fg7wyp1nlz2cfjx8p4phs586fxxabji5r082g5ixxsrq52b600"))))
-      ("yaml-src"
-       ,(origin
-	  (method url-fetch)
-	  (uri "https://github.com/ghodss/yaml/archive/v1.0.0.tar.gz")
-	  (sha256
-	   (base32
-	    "0i6yjwh3j2184lwwi537q7z666wppf5s1kz1m894d53is5yb8xla"))))
-      ("mux-src"
-       ,(origin
-	  (method url-fetch)
-	  (uri "https://github.com/gorilla/mux/archive/v1.3.0.tar.gz")
-	  (sha256
-	   (base32
-	    "0k6kgz9qgl6c1zd271hnzp96yh2p2v0410k4dq0y1j3qg2qfd7m1"))))
-      ("logrus-src"
-       ,(origin
-	  (method url-fetch)
-	  (uri "https://github.com/sirupsen/logrus/archive/v0.11.5.tar.gz")
-	  (sha256
-	   (base32
-	    "0skwgvd2d36y9fipjpbkzin9l9pnl2gk1i3xsf27497db5g5mgyj"))))
-      ("yaml.v2-src"
-       ,(origin
-	 (method git-fetch)
-	 (uri
-	  (git-reference
-	   ;; The url below gets the repo and checks out the
-	   ;; correct branch
-	   (url "https://gopkg.in/yaml.v2")
-	   (commit "cd8b52f8269e0feb286dfeef29f8fe4d5b397e0b")))
-	 (sha256
-	  (base32
-	   "1hj2ag9knxflpjibck0n90jrhsrqz7qvad4qnif7jddyapi9bqzl"))))
-      ("check.v1-src"
-       ,(origin
-	 (method git-fetch)
-	 (uri
-	  (git-reference
-	   (url "https://gopkg.in/check.v1")
-	   (commit "20d25e2804050c1cd24a7eea1e7a6447dd0e74ec")))
-	 (sha256
-	  (base32
-	   "0k1m83ji9l1a7ng8a7v40psbymxasmssbrrhpdv2wl4rhs0nc3np"))))))))
+    `(,(build-goamz-src)
+      ,(build-go-systemd-src)
+      ,(build-azure-sdk-for-go-src)
+      ,(build-yaml-src)
+      ,(build-mux-src)
+      ,(build-logrus-src)
+      ,(build-yaml.v2-src)
+      ,(build-check.v1-src)))))
 
 (define-public arvados-keep-balance
   (package
@@ -296,22 +309,5 @@ supports versioning, reproducibilty, and provenance.
    (propagated-inputs
     `(("go" ,go)))
    (inputs
-    `(("yaml-src"
-       ,(origin
-	 (method url-fetch)
-	 (uri "https://github.com/ghodss/yaml/archive/v1.0.0.tar.gz")
-	 (sha256
-	  (base32
-	   "0i6yjwh3j2184lwwi537q7z666wppf5s1kz1m894d53is5yb8xla"))))
-      ("yaml.v2-src"
-       ,(origin
-	 (method git-fetch)
-	 (uri
-	  (git-reference
-	   ;; The url below gets the repo and checks out the
-	   ;; correct branch
-	   (url "https://gopkg.in/yaml.v2")
-	   (commit "cd8b52f8269e0feb286dfeef29f8fe4d5b397e0b")))
-	 (sha256
-	  (base32
-	   "1hj2ag9knxflpjibck0n90jrhsrqz7qvad4qnif7jddyapi9bqzl"))))))))
+    `(,(build-yaml-src)
+      ,(build-yaml.v2-src)))))
